1d873eea8d4c22a838c4e91225077de13df30597
matheusaraujok <146781215+matheusaraujok@users.noreply.github.com>
2024-08-13 09:09:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple script to handle form submission
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for contacting us! We will get back to you soon.');
+});
